df5cf9ec6762668880f1d460c37b70eb5152a4e2
Robert Bantele <R.Bantele@live.de>
2020-04-01 23:27:21 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetVersionCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------ Installed .NET Versions below 4.5 ------------------------------");
+            GetVersionFromRegistry();
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------ Installed .NET Version above 4.5 ------------------------------");
+            Get45PlusFromRegistry();
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------ Installed .NET Core Version Info ------------------------------");
+            GetCoreVersion();
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------ Press any Key to exit ------------------------------");
+            Console.ReadKey();
+        }
+
+        private static void GetCoreVersion()
+        {
+            ExecuteCommandSync("dotnet --info");
+        }
+
+        /// <span class="code-SummaryComment"><summary></span>
+        /// Executes a shell command synchronously.
+        /// https://www.codeproject.com/Articles/25983/How-to-Execute-a-Command-in-C
+        /// <span class="code-SummaryComment"></summary></span>
+        /// <span class="code-SummaryComment"><param name="command">string command</param></span>
+        /// <span class="code-SummaryComment"><returns>string, as output of the command.</returns></span>
+        public static void ExecuteCommandSync(object command)
+        {
+            try
+            {
+                // create the ProcessStartInfo using "cmd" as the program to be run,
+                // and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                // and then exit.
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+
+                // The following commands are needed to redirect the standard output.
+                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = true;
+                // Now we create a process, assign its ProcessStartInfo and start it
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                // Get the output into a string
+                string result = proc.StandardOutput.ReadToEnd();
+                // Display the command output.
+                Console.WriteLine(result);
+            }
+            catch (Exception objException)
+            {
+                // Log the exception
+            }
+        }
+
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
+        /// </summary>
+        private static void Get45PlusFromRegistry()
+        {
+            const string subkey = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
+
+            using (var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(subkey))
+            {
+                if (ndpKey != null && ndpKey.GetValue("Release") != null)
+                {
+                    Console.WriteLine($".NET Framework Version: {CheckFor45PlusVersion((int)ndpKey.GetValue("Release"))}");
+                }
+                else
+                {
+                    Console.WriteLine(".NET Framework Version 4.5 or later is not detected.");
+                }
+            }
+
+            // Checking the version using >= enables forward compatibility.
+            string CheckFor45PlusVersion(int releaseKey)
+            {
+                if (releaseKey >= 528040)
+                    return "4.8 or later";
+                if (releaseKey >= 461808)
+                    return "4.7.2";
+                if (releaseKey >= 461308)
+                    return "4.7.1";
+                if (releaseKey >= 460798)
+                    return "4.7";
+                if (releaseKey >= 394802)
+                    return "4.6.2";
+                if (releaseKey >= 394254)
+                    return "4.6.1";
+                if (releaseKey >= 393295)
+                    return "4.6";
+                if (releaseKey >= 379893)
+                    return "4.5.2";
+                if (releaseKey >= 378675)
+                    return "4.5.1";
+                if (releaseKey >= 378389)
+                    return "4.5";
+                // This code should never execute. A non-null release key should mean
+                // that 4.5 or later is installed.
+                return "No 4.5 or later version detected";
+            }
+        }
+
+        // This example displays output like the following:
+        //       .NET Framework Version: 4.6.1
+        private static void GetVersionFromRegistry()
+        {
+            // Opens the registry key for the .NET Framework entry.
+            using (RegistryKey ndpKey =
+                    RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).
+                    OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                foreach (var versionKeyName in ndpKey.GetSubKeyNames())
+                {
+                    // Skip .NET Framework 4.5 version information.
+                    if (versionKeyName == "v4")
+                    {
+                        continue;
+                    }
+
+                    if (versionKeyName.StartsWith("v"))
+                    {
+
+                        RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
+                        // Get the .NET Framework version value.
+                        var name = (string)versionKey.GetValue("Version", "");
+                        // Get the service pack (SP) number.
+                        var sp = versionKey.GetValue("SP", "").ToString();
+
+                        // Get the installation flag, or an empty string if there is none.
+                        var install = versionKey.GetValue("Install", "").ToString();
+                        if (string.IsNullOrEmpty(install)) // No install info; it must be in a child subkey.
+                            Console.WriteLine($"{versionKeyName}  {name}");
+                        else
+                        {
+                            if (!(string.IsNullOrEmpty(sp)) && install == "1")
+                            {
+                                Console.WriteLine($"{versionKeyName}  {name}  SP{sp}");
+                            }
+                        }
+                        if (!string.IsNullOrEmpty(name))
+                        {
+                            continue;
+                        }
+                        foreach (var subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
+                            name = (string)subKey.GetValue("Version", "");
+                            if (!string.IsNullOrEmpty(name))
+                                sp = subKey.GetValue("SP", "").ToString();
+
+                            install = subKey.GetValue("Install", "").ToString();
+                            if (string.IsNullOrEmpty(install)) //No install info; it must be later.
+                                Console.WriteLine($"{versionKeyName}  {name}");
+                            else
+                            {
+                                if (!(string.IsNullOrEmpty(sp)) && install == "1")
+                                {
+                                    Console.WriteLine($"{subKeyName}  {name}  SP{sp}");
+                                }
+                                else if (install == "1")
+                                {
+                                    Console.WriteLine($"  {subKeyName}  {name}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // The example displays output similar to the following:
+        //        v2.0.50727  2.0.50727.4927  SP2
+        //        v3.0  3.0.30729.4926  SP2
+        //        v3.5  3.5.30729.4926  SP1
+        //        v4.0
+        //        Client  4.0.0.0
+    }
+}
